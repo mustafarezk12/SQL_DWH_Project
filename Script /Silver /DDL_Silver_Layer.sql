796805fff01907125dbd9495e883e872487ad679
Mustafa Rezk <152042329+mustafarezk12@users.noreply.github.com>
2025-05-03 18:13:13 +0300

    Update DDL_Silver_Layer.sql >> add metadata dwh_create_date

--- Script /Silver /DDL_Silver_Layer.sql
+++ Script /Silver /DDL_Silver_Layer.sql
@@ -1,55 +1,86 @@
---create cust_info table
-create table silver.crm_cust_info(
-cst_id int,
-cst_key nvarchar(50),
-cst_firstname nvarchar(50),
-cst_lastname nvarchar(50),
-cst_marital_status nvarchar(10) ,
-cst_gndr nvarchar(10),
-cst_create_date date)
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
 GO
---craete product table
-create table silver.crm_prd_inf(
-prd_id int,
-prd_key nvarchar(50),
-prd_nm nvarchar(50),
-prd_cost int,
-prd_line nvarchar(10) ,
-prd_start_dt date,
-prd_end_dt date)
-GO
---create sales table 
-create table silver.crm_Sales_details(
-sls_ord_num nvarchar(50),
-sls_prd_key nvarchar(50),
- sls_cust_id  INT,
-sls_order_dt INT,
-sls_ship_dt  INT,
-sls_due_dt   INT,
-sls_sales    INT,
-sls_quantity INT,
-sls_price    INT)
 
+CREATE TABLE silver.crm_cust_info (
+    cst_id             INT,
+    cst_key            NVARCHAR(50),
+    cst_firstname      NVARCHAR(50),
+    cst_lastname       NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr           NVARCHAR(50),
+    cst_create_date    DATE,
+    dwh_create_date    DATETIME2 DEFAULT GETDATE()
+);
 GO
---location table 
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
+
 CREATE TABLE silver.erp_loc_a101 (
-cid    NVARCHAR(50),
-cntry  NVARCHAR(50)
-)
-GO
---create category table 
-CREATE TABLE silver.erp_px_cat_g1v2 (
-id           NVARCHAR(50),
-cat          NVARCHAR(50),
-subcat       NVARCHAR(50),
-maintenance  NVARCHAR(50)
-)
+    cid             NVARCHAR(50),
+    cntry           NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
 
---
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
+
 CREATE TABLE silver.erp_cust_az12 (
-    cid    NVARCHAR(50),
-    bdate  DATE,
-    gen    NVARCHAR(50)
-)
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
 GO
